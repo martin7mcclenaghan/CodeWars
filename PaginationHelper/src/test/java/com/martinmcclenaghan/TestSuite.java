1819ebea4e9bbb4dc3c0b704418283d9cbc7886c
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-07-03 14:50:19 +0100

    Added TestSuite class to Pagination Helper.

---
+++ PaginationHelper/src/test/java/com/martinmcclenaghan/TestSuite.java
@@ -0,0 +1,17 @@
+package com.martinmcclenaghan;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+
+        PaginationHelperTest.class,
+        FurtherPaginationHelperTests.class
+
+})
+
+public class TestSuite {
+
+}
