713261648ddde07d1127c5f82b8fb3d55db3f61c
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-07-16 23:48:52 +0100

    Solution for Max Contiguous Sum.

---
+++ maxcontiguoussum/src/com/martinmcclenaghan/Main.java
@@ -0,0 +1,64 @@
+package com.martinmcclenaghan;
+
+//https://www.codewars.com/kata/maximum-contiguous-sum/train/java
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] start = new int[]{-8, -10, -12, -2, -3, 5};
+
+        System.out.println(maxContiguousSum(start));
+        System.out.println(maxContiguousSumAddition(start));
+
+
+    }
+
+    //below is based more heavily on Kadane's alogrithm
+    //https://en.wikipedia.org/wiki/Maximum_subarray_problem
+
+    public static int maxContiguousSumAddition(int[] array) {
+
+        for(int i = 1; i < array.length; i++){
+
+            if(array[i-1] > 0){
+
+                array[i] += array[i-1];
+            }
+        }
+
+        Arrays.sort(array);
+        return array[array.length-1];
+
+
+
+    }
+
+    //Based on a different implementation of Kadane's algorithm
+    //https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+
+    public static int maxContiguousSum(int[] array){
+
+        int localMax = 0;
+        int globalMax = Integer.MIN_VALUE;
+
+        for(int i = 0; i < array.length; i++){
+
+            localMax = Integer.max(array[i], array[i] + localMax);
+
+            if(localMax > globalMax){
+
+                globalMax = localMax;
+            }
+        }
+
+        return globalMax;
+
+
+    }
+
+
+}
