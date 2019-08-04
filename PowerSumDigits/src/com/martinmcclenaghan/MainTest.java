c960bb467ad7c8c1af17c44becb1fcc4112be9b0
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-04 23:50:38 +0100

    Set out test class now need to add cases.

--- PowerSumDigits/src/com/martinmcclenaghan/MainTest.java
+++ PowerSumDigits/src/com/martinmcclenaghan/MainTest.java
@@ -1,10 +1,37 @@
 package com.martinmcclenaghan;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
-@Runwith(Parameterized.class)
+@RunWith(Parameterized.class)
 
 public class MainTest {
 
+    private int input;
+    private long expected;
+
+    public MainTest(int input, long expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> testConditions (){
+        return Arrays.asList(new Object [][]{
+
+
+
+
+        });
+    }
+
+    @Test
+    public void powerSumDigTermTest (){
+        assertEquals(expected, Main.powerSumDigTerm(input));
+    }
 }
