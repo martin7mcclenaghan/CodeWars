52c486f0c48bc4f6b310e15c4859144cac2b0c17
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-18 00:23:47 +0100

    Added test cases but need to fix bug with highest card logic.

--- RankingPokerHands/src/test/java/com/martinmcclenaghan/PokerHandTest.java
+++ RankingPokerHands/src/test/java/com/martinmcclenaghan/PokerHandTest.java
@@ -28,6 +28,20 @@ public class PokerHandTest {
         return Arrays.asList(new Object[][]{
 
                 {PokerHand.Result.LOSS, "2H 3H 4H 5H 6H", "KS AS TS QS JS"},
+                {PokerHand.Result.WIN, "2H 3H 4H 5H 6H", "AS AD AC AH JD"},
+                {PokerHand.Result.WIN,"AS AH 2H AD AC", "JS JD JC JH 3D"},
+                {PokerHand.Result.LOSS,"2S AH 2H AS AC", "JS JD JC JH AD"},
+                {PokerHand.Result.WIN, "AS 3S 4S 8S 2S", "2H 3H 5H 6H 7H"},
+                {PokerHand.Result.WIN, "2H 3H 5H 6H 7H", "2S 3H 4H 5S 6C"},
+                {PokerHand.Result.TIE, "2S 3H 4H 5S 6C", "3D 4C 5H 6H 2S"},
+                {PokerHand.Result.WIN, "2S 3H 4H 5S 6C", "AH AC 5H 6H AS"},
+                {PokerHand.Result.LOSS, "2S 2H 4H 5S 4C", "AH AC 5H 6H AS"},
+                {PokerHand.Result.WIN, "2S 2H 4H 5S 4C", "AH AC 5H 6H 7S"},
+                {PokerHand.Result.LOSS, "6S AD 7H 4S AS", "AH AC 5H 6H 7S"},
+                {PokerHand.Result.LOSS, "2S AH 4H 5S KC", "AH AC 5H 6H 7S"},
+                {PokerHand.Result.LOSS, "2S 3H 6H 7S 9C", "7H 3C TH 6H 9S"},
+                {PokerHand.Result.WIN, "4S 5H 6H TS AC", "3S 5H 6H TS AC"},
+                {PokerHand.Result.TIE, "2S AH 4H 5S 6C", "AD 4C 5H 6H 2C"}
 
         });
     }
