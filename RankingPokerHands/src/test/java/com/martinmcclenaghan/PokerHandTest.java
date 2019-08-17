055b6e56c5a203b6a421ea8409abfb24bbfbd614
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-18 00:16:42 +0100

    Test class works but need to add more thorough cases.

--- RankingPokerHands/src/test/java/com/martinmcclenaghan/PokerHandTest.java
+++ RankingPokerHands/src/test/java/com/martinmcclenaghan/PokerHandTest.java
@@ -1,12 +1,44 @@
 package com.martinmcclenaghan;
 
+import junit.framework.TestResult;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class PokerHandTest {
+
+    private PokerHand.Result expectedResult;
+    private String playerDescription;
+    private String opponentDescription;
+
+    public PokerHandTest(PokerHand.Result expectedResult, String playerDescription, String opponentDescription) {
+        this.expectedResult = expectedResult;
+        this.playerDescription = playerDescription;
+        this.opponentDescription = opponentDescription;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> testConditions (){
+        return Arrays.asList(new Object[][]{
+
+                {PokerHand.Result.LOSS, "2H 3H 4H 5H 6H", "KS AS TS QS JS"},
+
+        });
+    }
 
     @Test
     public void compareWith() {
+        PokerHand playerHand = new PokerHand(playerDescription);
+        PokerHand opponentHand = new PokerHand(opponentDescription);
+        assertEquals(expectedResult, playerHand.compareWith(opponentHand));
+
     }
+
+
 }
