63accc0fe6ab8f10db57b894679884aad75ab5cb
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-17 23:55:27 +0100

    Added test class and changed project structure to Maven.

---
+++ RankingPokerHands/src/test/java/com/martinmcclenaghan/PokerHandTest.java
@@ -0,0 +1,12 @@
+package com.martinmcclenaghan;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PokerHandTest {
+
+    @Test
+    public void compareWith() {
+    }
+}
