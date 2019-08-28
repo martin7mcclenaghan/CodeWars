d8af198487ec6c333ecebd0722a72f79d48b82ad
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-08-28 23:59:06 +0100

    Test class for isHandFlush.

--- IsHandFlush/src/com/martinmcclenaghan/MainTest.java
+++ IsHandFlush/src/com/martinmcclenaghan/MainTest.java
@@ -1,8 +1,32 @@
 package com.martinmcclenaghan;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class MainTest {
+
+    private String[] input;
+    private boolean expected;
+
+    public MainTest(String[] input, boolean expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> testConditions (){
+        return Arrays.asList(new Object[][]{
+
+
+
+        });
+    }
 
     @org.junit.Test
     public void checkIfFlush() {
