caeef904f8e3036cff90edce1dbbec4858b4aad5
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-22 23:58:17 +0100

    Added further test cases for IsStraightTest.java.

--- PokerHandIsStraight/src/test/java/com/martinmcclenaghan/IsStraightTest.java
+++ PokerHandIsStraight/src/test/java/com/martinmcclenaghan/IsStraightTest.java
@@ -13,8 +13,8 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class IsStraightTest {
 
-    private int [] numbers;
-    private ArrayList <Integer> hand;
+    private int[] numbers;
+    private ArrayList<Integer> hand;
     private boolean expected;
 
     public IsStraightTest(int[] numbers, boolean expected) {
@@ -22,27 +22,29 @@ public class IsStraightTest {
         this.expected = expected;
 
         this.hand = new ArrayList<>();
-        for(int i : numbers){
+        for (int i : numbers) {
             this.hand.add(i);
         }
     }
 
     @Parameterized.Parameters
-    public static Collection<Object> testConditions (){
-        return Arrays.asList((Object []) new Object [][]{
+    public static Collection<Object> testConditions() {
+        return Arrays.asList((Object[]) new Object[][]{
 
-                {new int[] {7, 7, 12 ,11, 3, 4, 14}, false},
-
+                {new int[]{7, 7, 12, 11, 3, 4, 14}, false},
+                {new int[]{4, 5, 6, 7, 8}, true},
+                {new int []{2, 7, 12, 3, 4}, false},
+                {new int[] {10, 14, 14 ,13, 12, 10, 11}, true},
+                {new int[]{7, 8}, false}
 
         });
     }
 
     @Test
-    public void isStraightTest (){
+    public void isStraightTest() {
         assertEquals(expected, IsStraightMain.isStraight(this.hand));
 
     }
-
 
 
 }
