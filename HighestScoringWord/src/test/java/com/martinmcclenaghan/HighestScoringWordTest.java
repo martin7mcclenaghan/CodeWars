e046ea4d24b111478b386295bfab8da25e5df3a0
martin7mcclenaghan <48988919+martin7mcclenaghan@users.noreply.github.com>
2019-09-30 23:56:28 +0100

    HighestScoringWordTest.java created and set up. Need to add more test cases.

---
+++ HighestScoringWord/src/test/java/com/martinmcclenaghan/HighestScoringWordTest.java
@@ -0,0 +1,38 @@
+package com.martinmcclenaghan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class HighestScoringWordTest {
+
+    private String start;
+    private String expected;
+
+    public HighestScoringWordTest(String start, String expected) {
+        this.start = start;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object> testConditions (){
+        return Arrays.asList(new Object[][] {
+
+                {"what time are we climbing up to the volcano", "volcano"}
+
+        });
+    }
+
+    @Test
+    public void highTest (){
+
+        assertEquals(expected, HighestScoringWordMain.high(start));
+
+    }
+}
